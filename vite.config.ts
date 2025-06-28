3904ffe760101e8c3405c8f641d9c01fd26a9461
Alina Kosovetis <kosovetis@MacBook-Air-Kosovetis.local>
2025-06-28 10:46:22 +0700

    alias: enforce single react copy

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
+import { resolve } from "path";
 export default defineConfig({
   base: "./",          // ← ключевая строка!
-  resolve: { dedupe: ["react", "react-dom"] },
-  plugins: [react()],
-  build: { sourcemap: true }
+  resolve: {
+  dedupe: ["react", "react-dom"],
+  alias: {
+    react: resolve(__dirname, "node_modules/react"),
+    "react-dom": resolve(__dirname, "node_modules/react-dom")
+  }
+},
 });
