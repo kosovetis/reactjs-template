f7d35ddee148f527520ef8bf512e0fe3201e773e
Alina Kosovetis <kosovetis@MacBook-Air-Kosovetis.local>
2025-06-28 10:30:22 +0700

    fix: dedupe react copies

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   base: "./",          // ← ключевая строка!
+  resolve: { dedupe: ["react", "react-dom"] },
   plugins: [react()],
 });
