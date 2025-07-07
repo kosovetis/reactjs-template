c9d886383f49cbcaa5f6427b536c3b15bd86f310
Alina Kosovetis <kosovetis@MacBook-Air-Kosovetis.local>
2025-07-07 10:03:23 +0700

    Force light theme

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,17 +1,16 @@
 import { useMemo } from 'react';
 import { Navigate, Route, Routes, HashRouter } from 'react-router-dom';
-import { retrieveLaunchParams, useSignal, isMiniAppDark } from '@telegram-apps/sdk-react';
+import { retrieveLaunchParams } from '@telegram-apps/sdk-react';
 import { AppRoot } from '@telegram-apps/telegram-ui';
 
 import { routes } from '@/navigation/routes.tsx';
 
 export function App() {
   const lp = useMemo(() => retrieveLaunchParams(), []);
-  const isDark = useSignal(isMiniAppDark);
 
   return (
     <AppRoot
-      appearance={isDark ? 'dark' : 'light'}
+      appearance={'light'}
       platform={['macos', 'ios'].includes(lp.tgWebAppPlatform) ? 'ios' : 'base'}
     >
       <HashRouter>
