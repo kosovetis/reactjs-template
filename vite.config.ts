eb43272f9632758540bd7d62314f71ce2595ec86
Alina Kosovetis <kosovetis@MacBook-Air-Kosovetis.local>
2025-07-07 10:49:25 +0700

    Fix: Force light theme styles on Home and Rank component 2s

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,18 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { resolve } from "path";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: "./",          // ← ключевая строка!
+  base: './',
+  plugins: [react(), tsconfigPaths()],
+  // Добавляем пустой объект postcss, чтобы переопределить
+  // любую автоматическую конфигурацию и отключить Tailwind.
+  css: {
+    postcss: {}
+  },
   resolve: {
-  dedupe: ["react", "react-dom"],
-  alias: {
-    react: resolve(__dirname, "node_modules/react"),
-    "react-dom": resolve(__dirname, "node_modules/react-dom")
-  }
-},
+    alias: {
+    },
+  },
 });
