508ff99447194115aa81c8654764a75af6c46726
Alina Kosovetis <kosovetis@MacBook-Air-Kosovetis.local>
2025-08-18 23:32:24 +0700

    Add missing script tag for React app

--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,10 @@
 import { createRoot } from 'react-dom/client';
 import { StrictMode } from 'react';
-
-import { init } from '@/init';
-import { Root } from '@/components/Root';
-
+import Home from './Home';
 import './index.css';
-
-// Mock Telegram environment for development.
-import './mockEnv';
-
-const platform = new URLSearchParams(window.location.search).get('tgWebAppPlatform');
-
-// Initialize the SDK.
-init({
-  debug: import.meta.env.DEV,
-  eruda: import.meta.env.DEV,
-  // A bug in Telegram for macOS makes it necessary to mock the environment.
-  mockForMacOS: platform === 'macos',
-});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Root />
+    <Home />
   </StrictMode>,
 );
